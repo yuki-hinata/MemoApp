8a464e5bed3686f4de9232115548c5bf069db0d2
yuki-hinata <bigisland20001010@gmail.com>
2022-01-16 21:56:10 +0900

    Simple

--- App.jsx
+++ App.jsx
@@ -1,51 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
+
+import Appbar from './src/components/Appbar';
+import MemoList from './src/components/MemoList';
+import CircleButton from './src/components/CircleButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemdate}>2020年12月24日</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-      </View>
-
-      <View style={styles.memoListItem}>
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemdate}>2020年12月24日</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
-
-      <View style={styles.memoListItem}>
-        <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemdate}>2020年12月24日</Text>
-        </View>
-        <View>
-          <Text>X</Text>
-        </View>
-      </View>
-
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
+      <Appbar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
     </View>
   );
 }
@@ -54,67 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  memoListItem: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemdate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
-  },
-  circleButton: {
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000000',
-    textShadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  },
-  circleButtonLabel: {
-    color: '#fff',
-    fontSize: 40,
-    lineHeight: 40,
   },
 });
